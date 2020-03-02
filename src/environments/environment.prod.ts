be3088d4b0d6c09b4632fa97b6794b16dbc57df0
Prudhvi Godavarthi <M1046935@mindtree.com>
2020-03-02 19:05:14 +0530

    added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  serverUrl: 'http://13.58.47.119:8080'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  serverUrl: 'http://18.217.240.130:8080'
+};
+
+
